97294d032e9f4e704ebfca4ea9e7a340e4dfda39
RIML99 <isaacfenix123@gmail.com>
2022-03-10 17:29:07 -0600

    Actuaizacion de SW4

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 //confirmar si soporta sw
 if ( navigator.serviceWorker ){
     //alert("Soporta la que puede"):
-    navigator.serviceWorker.register("/sw.js");
+    navigator.serviceWorker.register("sw.js");
 }
 else{
     alert(": No soporto... tampoco el SW")
