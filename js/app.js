5a19114b7c28d288937da1367462cd8ae706c885
RIML99 <100782231+RIML99@users.noreply.github.com>
2022-04-12 18:19:44 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 //confirmar si soporta sw
 if ( navigator.serviceWorker ){
     //alert("Soporta la que puede"):
-    navigator.serviceWorker.register("Demo_Isaac/sw.js");
+    navigator.serviceWorker.register("/Demo_Isaac/sw.js");
 }
 else{
     alert(": No soporto... tampoco el SW")
