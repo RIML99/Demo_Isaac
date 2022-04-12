25618d67685d74d36b0f6ec7b65d5bb3d46da762
RIML99 <100782231+RIML99@users.noreply.github.com>
2022-04-12 18:36:41 -0500

    cambio otra vez sw

--- sw_offline.js
+++ sw_offline.js
@@ -15,7 +15,7 @@ self.addEventListener('fetch' , event => {
         <meta charset="UTF-8">
          <meta name="viewport" content="width=device-width, initial-scale=1.0">
          <meta http-equiv="X-UA-Compatible" content="ie=edge">
-         <title>Kross - Cruz Alvarado Irvin Jonathan Template</title>
+         <title>MendozaLopezRicardoIsaac</title>
       </head>
       <body>        
         <h1> Bienvenido a nuestra pagina web
